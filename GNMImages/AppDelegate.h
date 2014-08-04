e1ecfe031bc4a75a7b0034ae1ea71ee529ff0511
localhome <localhome@andysmuamacbook.gnm.int>
2014-08-04 15:01:48 +0100

    Initial Commit

---
+++ GNMImages/AppDelegate.h
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.h
+//  GNMImages
+//
+//  Created by localhome on 04/08/2014.
+//  Copyright (c) 2014 Guardian News & Media. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+
+- (IBAction)saveAction:(id)sender;
+
+@end
---
+++ GNMImages/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  GNMImages
+//
+//  Created by localhome on 04/08/2014.
+//  Copyright (c) 2014 Guardian News & Media. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+    return NSApplicationMain(argc, argv);
+}
